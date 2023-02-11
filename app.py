48c8d187121271728b9d5ace63a5b20ac5f60382
Daron DuBose <awesome9947@gmail.com>
2023-02-11 01:12:24 -0500

    added: example customer service commands

--- app.py
+++ app.py
@@ -27,13 +27,18 @@ while True:
     except:
         continue
 
-    if "do something" in user_input.lower():
-        response_str = "Playing a sound."
+    if "what is your return policy" in user_input.lower():
+        response_str = "Our return policy is that you can return items within 30 days of purchase for a full refund. Please make sure the item is in its original condition and packaging."
 
-    if user_input.startswith("whisper"):
-        whisper_input = user_input.replace("whisper", "", 1)
-        response_str = whisper_input + "\n(whispered)"
-        
+    if "where is my order" in user_input.lower():
+        response_str = "You can check the status of your order by logging into your account on our website or by contacting us at (insert customer service number here). We'll be happy to help you track down your shipment."
+
+    if "how do I contact support" in user_input.lower():
+        response_str = "You can reach our customer support team by phone at (insert customer service number here) or by email at (insert customer service email address here). We're available from 9am to 5pm PST, Monday to Friday."
+
+    if "I want to cancel my order" in user_input.lower():
+        response_str = "If you'd like to cancel your order, please contact our customer support team as soon as possible. They'll be able to assist you in cancelling your order and processing a refund if necessary."
+
     else:
         prompt = user_name + ": " + user_input + "\nBot:" 
         conversation += prompt
