3886e40fc35b97de06b690832912d2bbb027efa2
Daron DuBose <awesome9947@gmail.com>
2023-02-11 11:35:48 -0500

    added: thefuzz

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import openai
 import speech_recognition as sr
 import os
 import playsound
+from thefuzz import fuzz
 from voice import speak
 
 openai.api_key = "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"
@@ -27,16 +28,16 @@ while True:
     except:
         continue
 
-    if "what is your return policy" in user_input.lower():
+    if fuzz.token_set_ratio(user_input.lower(), "what is your return policy") >= 60:
         response_str = "Our return policy is that you can return items within 30 days of purchase for a full refund. Please make sure the item is in its original condition and packaging."
 
-    if "where is my order" in user_input.lower():
+    elif fuzz.token_set_ratio(user_input.lower(), "where is my order") >= 60:
         response_str = "You can check the status of your order by logging into your account on our website or by contacting us at (insert customer service number here). We'll be happy to help you track down your shipment."
 
-    if "how do I contact support" in user_input.lower():
+    elif fuzz.token_set_ratio(user_input.lower(), "how do I contact support") >= 60:
         response_str = "You can reach our customer support team by phone at (insert customer service number here) or by email at (insert customer service email address here). We're available from 9am to 5pm PST, Monday to Friday."
 
-    if "I want to cancel my order" in user_input.lower():
+    elif fuzz.token_set_ratio(user_input.lower(), "I want to cancel my order") >= 60:
         response_str = "If you'd like to cancel your order, please contact our customer support team as soon as possible. They'll be able to assist you in cancelling your order and processing a refund if necessary."
 
     else:
