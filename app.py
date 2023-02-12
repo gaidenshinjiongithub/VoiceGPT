d1a59ec46f750846ffaa495e4e8b0505676efd95
Daron DuBose <awesome9947@gmail.com>
2023-02-12 15:35:32 -0500

    added: repeat to if statements

--- app.py
+++ app.py
@@ -28,16 +28,16 @@ while True:
     except:
         continue
 
-    if fuzz.token_set_ratio(user_input.lower(), "what is your return policy") >= 60:
+    if fuzz.token_set_ratio(user_input.lower(), "what is your return policy") >= 60 or fuzz.token_set_ratio(user_input.lower(), "repeat") >= 60:
         response_str = "Our return policy is that you can return items within 30 days of purchase for a full refund. Please make sure the item is in its original condition and packaging."
 
-    elif fuzz.token_set_ratio(user_input.lower(), "where is my order") >= 60:
+    elif fuzz.token_set_ratio(user_input.lower(), "what is your return policy") >= 60 or fuzz.token_set_ratio(user_input.lower(), "repeat") >= 60:
         response_str = "You can check the status of your order by logging into your account on our website or by contacting us at (insert customer service number here). We'll be happy to help you track down your shipment."
 
-    elif fuzz.token_set_ratio(user_input.lower(), "how do I contact support") >= 60:
+    elif fuzz.token_set_ratio(user_input.lower(), "what is your return policy") >= 60 or fuzz.token_set_ratio(user_input.lower(), "repeat") >= 60:
         response_str = "You can reach our customer support team by phone at (insert customer service number here) or by email at (insert customer service email address here). We're available from 9am to 5pm PST, Monday to Friday."
 
-    elif fuzz.token_set_ratio(user_input.lower(), "I want to cancel my order") >= 60:
+    elif fuzz.token_set_ratio(user_input.lower(), "what is your return policy") >= 60 or fuzz.token_set_ratio(user_input.lower(), "repeat") >= 60:
         response_str = "If you'd like to cancel your order, please contact our customer support team as soon as possible. They'll be able to assist you in cancelling your order and processing a refund if necessary."
 
     else:
